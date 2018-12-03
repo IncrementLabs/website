3388792b281796a104225313a51bcd656c6c67b0
Mark Miyashita <negativetwelve@gmail.com>
2018-12-02 22:15:46 -0800

    Remove unused components from the Home page

--- src/views/Home/components/index.js
+++ src/views/Home/components/index.js
@@ -1,5 +1,3 @@
-export {default as ComingSoonSection} from './ComingSoonSection';
-export {default as GetStartedSection} from './GetStartedSection';
 export {default as HeroSection} from './HeroSection';
 export {default as ProductFeature} from './ProductFeature';
 export {default as ProductSection} from './ProductSection';
